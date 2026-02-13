6ee3921e27222ed17999e18743d658d6b69f65fb
Andrea Virgil Ofian <andreavirgil89@gmail.com>
2026-02-14 05:56:29 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+const themeToggleBtn = document.querySelector(".theme-toggler");
+
+buttons.forEach((item) => {
+  item.onclick = () => {
+    if (item.id == "clear") {
+      display.innerText = "";
+    } else if (item.id == "backspace") {
+      let string = display.innerText.toString();
+      display.innerText = string.substr(0, string.length - 1);
+    } else if (display.innerText != "" && item.id == "equal") {
+      try {
+        display.innerText = eval(display.innerText);
+      } catch {
+        display.innerText = "Error";
+        setTimeout(() => (display.innerText = ""), 2000);
+      }
+    } else if (display.innerText == "" && item.id == "equal") {
+      display.innerText = "Empty";
+      setTimeout(() => (display.innerText = ""), 2000);
+    } else {
+      display.innerText += item.id;
+    }
+  };
+});
+
+themeToggleBtn.onclick = () => {
+  document.body.classList.toggle("dark");
+  themeToggleBtn.classList.toggle("active");
+};
